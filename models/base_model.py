d94e42363b467fb2369e2047385ecc58c4f7987b
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-10 20:39:57 -0800

    added tests/test_base_model.py, models/base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """BaseModel class for all other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return string representation of BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at attribute with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary representation of BaseModel instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
